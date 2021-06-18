9dbafeeb3e01e2ffec91f7fe2294c4dc6ebc4f33
Taiene Yuki <taieneyamazaki@gmail.com>
2021-06-18 15:00:32 -0300

    primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const champsData = data.data;
+
+let arrayChamps = [];
+let index;
+for (index in champsData) {
+  arrayChamps.push(champsData[index]);
+}
+
+arrayChamps.map(function(){
+  
+})
+
+
+console.log(arrayChamps);
+//console.log(Object.keys(info));
