c406dab2dd05c9102daf9da8d200cdaffc39b581
tamaracosta <tamarasantosc@gmail.com>
2021-06-30 14:21:08 -0300

    Atualização funções, curiosidade, data.js

---
+++ src/curiosidade.js
@@ -0,0 +1,22 @@
+import data from './data/lol/lol.js';
+import { dadosLol } from './data.js';
+
+const champs = Object.values(data.data);
+
+// VIRAR CARD CURIOSIDADE
+function virarCardCuriosidade() {
+    const botaoDescubra = document.querySelector("#cardCuriosidade")
+    botaoDescubra.addEventListener("click", (e) => {
+        botaoDescubra.classList.toggle('flip');
+    })
+
+}
+document.getElementById("descubra").addEventListener("click", virarCardCuriosidade)
+
+
+
+// CRIAR FUNCAO PARA CALCULAR O NIVEL DE DIFICULDADE DOS PERSONAGENS
+let resultadoFinal = dadosLol.computeStats(champs)
+document.getElementById('resultadoMedia').innerHTML = "A média é: " + resultadoFinal
+
+
