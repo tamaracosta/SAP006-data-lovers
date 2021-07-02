de14d25e79585fc9ae2f5d2b1d584e95150512c6
tamaracosta <tamarasantosc@gmail.com>
2021-07-02 14:08:50 -0300

    funcao esconder card curiosidade

--- src/curiosidade.js
+++ src/curiosidade.js
@@ -3,16 +3,22 @@ import { dadosLol } from './data.js';
 
 const champs = Object.values(data.data);
 
+// ESCONDER CARD FRENTE QUANDO VIRAR
+function esconderCardFrente() {
+  const cardFrente = document.getElementById("cardCuriosidadeFrente")
+  setTimeout(function(){ cardFrente.style.display = "none"; }, 1000);
+}
+
+
 // VIRAR CARD CURIOSIDADE
 function virarCardCuriosidade() {
     const botaoDescubra = document.querySelector("#cardCuriosidade")
-    botaoDescubra.addEventListener("click", (e) => {
-        botaoDescubra.classList.toggle('flip');
-    })
+    botaoDescubra.classList.toggle('flip');
+
+    esconderCardFrente()
 
 }
 document.getElementById("descubra").addEventListener("click", virarCardCuriosidade)
-
 
 
 // CRIAR FUNCAO PARA CALCULAR O NIVEL DE DIFICULDADE DOS PERSONAGENS
