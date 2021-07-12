5457b64e948feb87447d690cf96031bd08ed59e4
Taiene Yuki <taieneyamazaki@gmail.com>
2021-07-12 14:51:42 -0300

    testes computeStats

--- src/data.js
+++ src/data.js
@@ -1,42 +1,36 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+
+
+export const filterData = (data, condition) => {
+  return data.filter(condition);
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
 
-export const dadosLol = {
-  filterData:
-    function (data, condition) {
-      return data.filter(condition);
-    },
-
-  sortData:
-    function (data, sortBy, sortOrder) {
-
-      sortOrder == "crescente" ?
-        data.sort((a, b) => { return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0 }) :
-        data.sort((a, b) => { return a[sortBy] > b[sortBy] ? -1 : a[sortBy] < b[sortBy] ? 1 : 0 })
-
-    },
-
-  computeStats:
-    function (data) {
-
-      let totalDificuldade = 0
-      let totalPersonagens = data.length
-      let resultadoFinal;
-
-      for (let i = 0; i < data.length; i++) {
-        totalDificuldade += data[i].info.difficulty
-      }
-
-      resultadoFinal = totalDificuldade / totalPersonagens
-      return resultadoFinal.toFixed(2);
-
-    }
+export const sortData = (data, sortBy, sortOrder) => {
+  sortOrder == "crescente" ?
+    data.sort((a, b) => { return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0 }) :
+    data.sort((a, b) => { return a[sortBy] > b[sortBy] ? -1 : a[sortBy] < b[sortBy] ? 1 : 0 })
 }
+
+export const computeStats = (data) => {
+  if (Array.isArray(data) === false) {
+    throw TypeError();
+  }
+  const soma = data.reduce((acumulado, atual) => acumulado + atual);
+  const divisor = data.length;
+  const media = (soma) / divisor;
+
+  return media.toFixed(2);
+}
+
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,20 @@
-import { example, anotherExample } from '../src/data.js';
+import {  computeStats } from '../src/data.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('computeStats', () => {
+  it('deve ser uma função', () => {
+    expect(typeof computeStats).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+  it('deve jogar um TypeError quando invocado com tipos de argumentos errados', () => {
+    expect(() => computeStats(4)).toThrow(TypeError); //argumento tipo number
+    expect(() => computeStats('string')).toThrow(TypeError); //argumento tipo string
+    expect(() => computeStats()).toThrow(TypeError); //argumento vazio 
+    expect(() => computeStats(null)).toThrow(TypeError); //argumento null
+    expect(() => computeStats({})).toThrow(TypeError);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+  it('deve retornar media 8.00 para data [7,10,7]', () => {
+    expect(computeStats([7,10,7])).toBe('8.00');
+  })
+})
+
