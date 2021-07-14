3bb55b998c972ef1eb5cff6243b600704925d74e
Taiene Yuki <taieneyamazaki@gmail.com>
2021-07-14 14:08:28 -0300

    testes unitarios/ filter

--- src/data.js
+++ src/data.js
@@ -1,22 +1,33 @@
 export const filterData = (data, condition) => {
   return data.filter(condition);
- }
-  
- export const sortData = (data, sortBy, sortOrder) => {
-  
-  return sortOrder == "crescente" ?
-    data.sort((a, b) => { return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0 }) :
+}
+
+export const sortData = (data, sortBy, sortOrder) => {
+  return sortOrder === "crescente" ?
+    data.sort((a, b) => { return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0 }):
     data.sort((a, b) => { return a[sortBy] > b[sortBy] ? -1 : a[sortBy] < b[sortBy] ? 1 : 0 })
-    
- }
-  
- export const computeStats = (data) => {
+}
+
+export const computeStats = (data) => {
   if (Array.isArray(data) === false) {
     throw TypeError();
   }
   const soma = data.reduce((acumulado, atual) => acumulado + atual, 0);
   const divisor = data.length;
   const media = parseInt(soma) / divisor;
-   return media.toFixed(2);
- }
- 
+  return media.toFixed(2);
+}
+
+export const filterNestedObj = (data, selected, obj) => {
+  if (typeof selected !== "string" && Array.isArray(selected) === false || Array.isArray(data) === false || typeof obj !== "function") {
+    throw TypeError();
+  }
+  return data.filter(item => selected.includes(obj(item)));
+}
+
+export const filterNestedArr = (data, propriedade, selected) => {
+  if (typeof selected !== "string" && Array.isArray(selected) === false || Array.isArray(data) === false) {
+    throw TypeError();
+  }
+  return data.filter(item => item[propriedade].some(s => selected.includes(s)));
+}
